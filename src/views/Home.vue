abb643fde0d83f76c7e6c5e8d822d97f9fb341ea
Bün Colak <bunyamincolak@gmail.com>
2020-12-20 23:37:34 +0100

    apollo conf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,26 @@
 <template>
   <div class="bg-gray-400">
-    hello there
+    <div v-for="user in users" :key="user.id">{{ user.username }}</div>
   </div>
 </template>
 
 <script>
+import gql from 'graphql-tag'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  mounted () {
+    console.log(this.users)
+  },
+  apollo: {
+    users: gql`
+      {
+        users {
+          id
+          username
+        }
+      }
+    `
+  }
 }
 </script>
